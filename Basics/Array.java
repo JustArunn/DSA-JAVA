1ba7b0f938f743c623b014b6b96ad797fee69fbb
Arun Kumar <arun.kumar@cubiclogics.com>
2024-09-20 20:24:43 +0530

    java dsa

--- Basics/Array.java
+++ Basics/Array.java
@@ -4,18 +4,19 @@ import java.util.Scanner;
 
 public class Array {
     public static void main(String[] args) {
-        int[] arr = {1, 2,3 ,4, 5};
-        for(int i = 0; i < 5; i++){
+        int[] arr = { 1, 2, 3, 4, 5 };
+        for (int i = 0; i < 5; i++) {
             System.out.print(arr[i] + " ");
         }
 
         System.out.println("taking input in array");
         int[] arr2 = new int[5];
-        Scanner in = new Scanner(System.in);
-        for(int i = 0; i < 5; i++) arr2[i] = in.nextInt();
-
+        try (Scanner in = new Scanner(System.in)) {
+            for (int i = 0; i < 5; i++)
+                arr2[i] = in.nextInt();
+        }
         System.out.println("Printing Array of input");
-        for(int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             System.out.print(arr2[i] + " ");
         }
     }
